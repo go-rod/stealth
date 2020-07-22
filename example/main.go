d73eb5dec408aa2bdcc8be441b2360af4a37744b
Yad Smood <ys@ysmood.org>
2020-07-22 21:13:42 +0900

    add golang api

--- example/main.go
+++ example/main.go
@@ -1,72 +1,21 @@
 package main
 
 import (
+	"bypass"
 	"fmt"
+
 	"github.com/go-rod/rod"
-	"github.com/go-rod/rod/lib/launcher"
-	"github.com/go-rod/rod/lib/proto"
-	"io/ioutil"
 )
 
 func main() {
-	js, err := fetchBypassJS()
-	if err != nil {
-		panic(err)
-	}
+	browser := rod.New().Connect()
+	defer browser.Close()
 
-	if len(js) == 0 {
-		panic("js empty")
-	}
+	page := bypass.Page(browser)
 
-	l := launcher.New().Launch()
+	page.Navigate("https://bot.sannysoft.com").WaitLoad()
 
-	b := rod.New().ControlURL(l).Connect()
-	defer b.Close()
-
-	p := b.Page("")
-
-	_, err = proto.PageAddScriptToEvaluateOnNewDocument{
-		Source: js,
-	}.Call(p)
-	if err != nil {
-		panic(err)
-	}
-
-	// p.Emulate(devices.Pixel2XL)
-	// puppeteer:protocol SEND ► {"sessionId":"AEEF67CFDF10EEF9A0013C44E73EF2C1","method":"Network.setUserAgentOverride","params":{"userAgent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3882.0 Safari/537.36","acceptLanguage":"en-US,en","platform":"Win32"},"id":27}
-	err = proto.NetworkSetUserAgentOverride{
-		UserAgent:      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3882.0 Safari/537.36",
-		AcceptLanguage: "en-US,en",
-		Platform:       "Win32",
-	}.Call(p)
-	if err != nil {
-		panic(err)
-	}
-
-	p.Navigate("https://bot.sannysoft.com")
-
-	p.WaitRequestIdle()()
-
-	p.ScreenshotFullPage("testresult.png")
+	page.ScreenshotFullPage("testresult.png")
 
 	fmt.Println("All done, check the screenshot. ✨")
-
-}
-
-func fetchBypassJS() (js string, err error) {
-	//resp, err := http.Get("https://cdn.jsdelivr.net/gh/go-rod/bypass@latest/puppeteer-dump/dist/stealth.js")
-	//if err != nil {
-	//	return
-	//}
-	//
-	//defer resp.Body.Close()
-	//b, err := ioutil.ReadAll(resp.Body)
-
-	b, err := ioutil.ReadFile("puppeteer-dump/dist/stealth.js")
-	if err != nil {
-		return
-	}
-
-	js = string(b)
-	return
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module bypass
 
 go 1.14
 
-require github.com/go-rod/rod v0.48.2
+require (
+	github.com/go-rod/rod v0.48.2
+	github.com/ysmood/kit v0.24.6
+)
