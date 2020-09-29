a7d34c5312777fbb4381e93fb5d8ff5188bc6a0e
Yad Smood <ys@ysmood.org>
2020-09-29 10:06:25 +0900

    update js

--- examples_test.go
+++ examples_test.go
@@ -24,7 +24,7 @@ func Example_main() {
 	/*
 		Output:
 
-		js file size: 112379
+		js file size: 112395
 
 		User Agent (Old): Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36
 
