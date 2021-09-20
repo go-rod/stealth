f635a34ad526058df4974b1f9f263b942f5fa457
Yad Smood <y.s.inside@gmail.com>
2021-09-20 09:46:28 +0800

    make the test more robust

--- examples_test.go
+++ examples_test.go
@@ -53,8 +53,6 @@ func Example_main() {
 
 		WebGL Renderer: Intel Iris OpenGL Engine
 
-		Hairline Feature: missing
-
 		Broken Image Dimensions: 16x16
 	*/
 }
@@ -66,6 +64,10 @@ func printReport(page *rod.Page) {
 		key := cells[0].MustProperty("textContent")
 		if strings.HasPrefix(key.String(), "User Agent") {
 			fmt.Printf("\t\t%s: %t\n\n", key, !strings.Contains(cells[1].MustProperty("textContent").String(), "HeadlessChrome/"))
+		} else if strings.HasPrefix(key.String(), "Hairline Feature") {
+			// Detects support for hidpi/retina hairlines, which are CSS borders with less than 1px in width, for being physically 1px on hidpi screens.
+			// Not all the machine suppports it.
+			continue
 		} else {
 			fmt.Printf("\t\t%s: %s\n\n", key, cells[1].MustProperty("textContent"))
 		}
